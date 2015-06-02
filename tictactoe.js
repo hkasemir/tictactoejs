5c9c75484f024ac6801971f8e9d8cf7a5c1c80ea
hkasemir <heidi.kasemir@gmail.com>
2015-06-02 16:28:37 -0400

    got the js logic working

--- tictactoe.js
+++ tictactoe.js
@@ -1,23 +1,17 @@
-// function getUserInput(question, callback) {
-//  var readline = require('readline');
-//
-//  var rl = readline.createInterface({
-//    input: process.stdin,
-//    output: process.stdout
-//  });
-//
-//  rl.question(question, function(answer) {
-//    callback(answer);
-//    rl.close();
-//  });
-//};
-
 
 
 BOARD_SIZE = 3
 EMPTY_KEY = ' '
 PLAYER_KEY = 'x'
 COMPUTER_KEY = 'o'
+IN_A_ROW_TO_WIN = 3
+DIRECTIONS = [
+    [0, 1],      // Check to the right
+    [1, 0],      // Check down the column
+    [1, 1],      // Check diagonal to the right
+    [1, -1]      // Check diagonal to the left
+]
+
 
 
 function createBoard(size){
@@ -64,25 +58,76 @@ function stringBoard(board){
   return strBoard
 };
 
+function computerMove(board){
+  var row = 1, column = 1, esc = 0;
+  while (board[row][column] != 0){
+    column = Math.floor(Math.random() * board.length);
+    console.log(' computed column: ' + column)
+    row = Math.floor(Math.random() * board.length);
+    console.log(' computed row: ' + row)
+    esc += 1;
+    if (esc >=20){
+      console.log('too many while loops');
+      break
+    }
+  }
+  board[row][column] = -1;
+}
 
+function checkDirection(board, pos, dir){
+  var char = board[pos[0]][pos[1]];
+  if(char === 0){return false}
+  checkSum = 1;
+  while (true){
+    pos = [pos[0] + dir[0], pos[1] + dir[1]];
+    if (pos[0] >= board.length || pos[1] >= board.length){
+      break
+    }
+    var nextChar = board[pos[0]][pos[1]];
+    if (nextChar != char){
+      break
+    }
+    checkSum += 1;
+    if (checkSum === IN_A_ROW_TO_WIN){
+      return true
+    }
+  }
+  return false
+}
 
-//function getPlayerColumn(gameboard){
-//  getUserInput("Choose a column (1, 2, etc.): ", function(col){
-//    var colInt
-//    if(Number.isInteger(parseFloat(col))){
-//      colInt = parseInt(col)
-//    };
-//    if (colInt && colInt < gameboard.length + 1 && colInt >= 0){
-//      console.log('yep');
-//    } else {
-//      console.log('nope')
-//    };
-//  });
-//};
+function checkWin(board){
+  for (var row = 0; row < board.length; row++){
+    for (var col = 0; col < board.length; col++){
+      var pos = [row, col];
+      for (var dir = 0; dir < DIRECTIONS.length; dir++){
+        if (checkDirection(board, pos, DIRECTIONS[dir])){
+          return true
+        }
+      }
+    }
+  }
+  for (var row = 0; row < board.length; row++){
+    for (var col = 0; col < board.length; col++){
+      if (board[row][col] === 0){
+        return false
+      }
+    }
+  }
+  return undefined
+};
+          
+
 
 
 
 var board = createBoard(BOARD_SIZE);
 console.log(stringBoard(board));
-
+computerMove(board);
+console.log(board)
+computerMove(board);
+console.log(board)
+computerMove(board);
+console.log(board)
+computerMove(board);
+console.log(board)
 
